9d88c0c5b3b2133e2ef2c13e1baf730092dc15ee
Lazar <lazazr@gmail.com>
2020-01-31 19:39:57 +0100

    fix: fix cors when in prod ?

--- server/config/cors.js
+++ server/config/cors.js
@@ -19,7 +19,7 @@ module.exports = {
   origin: (origin) => {
     if (process.env.NODE_ENV === 'development') return true
 
-    if (origin.includes('lunch-money.netlify.com/')) return true
+    if (origin.includes('https://lunch-money.netlify.com')) return true
 
     return false
   },
